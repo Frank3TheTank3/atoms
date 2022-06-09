88f072aa172a8b3610b83a6eb432107236b0c053
Frank3TheTank3 <francisnic90@gmail.com>
2022-06-09 12:17:46 +0200

    Add icon components and hero image with licenses
    Updated clipart and background elements

---
+++ resources/images/licences.md
@@ -0,0 +1,30 @@
+
+***Creazilla Clipart:***
+
+https://creazilla.com/
+Attribution link: publicdomainq.net
+License:
+Public Domain. Free for personal and commercial use. No attribution required.
+
+
+***Undraw clipart:***
+
+https://undraw.co/license
+
+full license text
+Copyright 2022 Katerina Limpitsouni
+
+All images, assets and vectors published on unDraw can be used for free. You can use them for noncommercial and commercial purposes. You do not need to ask permission from or provide credit to the creator or unDraw.
+
+More precisely, unDraw grants you an nonexclusive, worldwide copyright license to download, copy, modify, distribute, perform, and use the assets provided from unDraw for free, including for commercial purposes, without permission from or attributing the creator or unDraw. This license does not include the right to compile assets, vectors or images from unDraw to replicate a similar or competing service, in any form or distribute the assets in packs or otherwise. This extends to automated and non-automated ways to link, embed, scrape, search or download the assets included on the website without our consent.
+
+***Background-main:***
+
+https://www.freepik.com/free-vector/atomic-science-technology-background-vector-border-blue-neon-style-with-blank-space_17213335.htm#query=science%20atom&position=0&from_view=keyword
+
+Attribution:
+
+<a href="https://www.freepik.com/vectors/copy-space">Copy space vector created by rawpixel.com - www.freepik.com</a>
+
+***Licenes List:***
+https://spdx.org/licenses/
---
+++ resources/js/components/icons/ClipartBooks.vue
@@ -0,0 +1,51 @@
+<template>
+    <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 344.8 356.1" style="enable-background:new 0 0 344.8 356.1;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill-rule:evenodd;clip-rule:evenodd;fill:#1F4A57;}
+	.st1{fill-rule:evenodd;clip-rule:evenodd;fill:#3D7385;}
+	.st2{fill-rule:evenodd;clip-rule:evenodd;fill:#8AA1A6;}
+	.st3{fill-rule:evenodd;clip-rule:evenodd;fill:#F5C745;}
+	.st4{fill-rule:evenodd;clip-rule:evenodd;fill:#F0522E;}
+	.st5{fill-rule:evenodd;clip-rule:evenodd;fill:#F7D475;}
+	.st6{fill-rule:evenodd;clip-rule:evenodd;fill:#3B9987;}
+	.st7{fill-rule:evenodd;clip-rule:evenodd;fill:#FFD463;}
+	.st8{fill-rule:evenodd;clip-rule:evenodd;fill:#ED5445;}
+	.st9{fill-rule:evenodd;clip-rule:evenodd;fill:#FFE396;}
+	.st10{fill-rule:evenodd;clip-rule:evenodd;fill:#FF8061;}
+	.st11{fill-rule:evenodd;clip-rule:evenodd;fill:#4AAD99;}
+</style>
+<g>
+	<path class="st0" d="M63.2,70.3v282.2c0,0.9,0.7,1.7,1.7,1.7h89.9c1,0,1.7-0.8,1.7-1.7V70.3c0-1-0.7-1.7-1.7-1.7H65
+		C64,68.6,63.2,69.3,63.2,70.3"/>
+	<path class="st1" d="M139.1,353.6l3.7-0.1c5.9-0.1,10.7-4.9,10.7-10.8V83.1c0-5.8-4.8-10.6-10.7-10.5l-3.7,0.1
+		c-5.8,0.1-10.6,4.9-10.6,10.8v259.6C128.5,349,133.3,353.7,139.1,353.6"/>
+	<rect x="63.4" y="286.6" class="st2" width="93.2" height="16.3"/>
+	<path class="st3" d="M0.1,25.9v323.6c0,3.1,2.5,5.6,5.6,5.6h52.2c3.1,0,5.6-2.5,5.6-5.6V25.9c0-3.1-2.5-5.6-5.6-5.6H5.7
+		C2.6,20.3,0.1,22.8,0.1,25.9"/>
+	<rect x="167.5" class="st4" width="76.8" height="354.5"/>
+	<path class="st5" d="M216.9,46.1V267c0,6.1-4.5,11.2-10.1,11.2c-5.7,0-10.2-5-10.2-11.2V46.1c0-6.2,4.5-11.2,10.2-11.2
+		C212.3,34.9,216.9,39.9,216.9,46.1"/>
+	<rect x="64.2" y="238.5" class="st2" width="93.2" height="16.3"/>
+	<path class="st6" d="M244,148.9l39.3,201.8c0.7,3.7,4.2,6,7.9,5.3l48.3-9.4c3.6-0.7,6-4.2,5.3-7.9l-39.3-201.8
+		c-0.7-3.6-4.2-6-7.9-5.3l-48.3,9.4C245.7,141.7,243.3,145.2,244,148.9"/>
+	<path class="st7" d="M51.4,352.1L51.4,352.1c4.4,0.9,7.9-3.1,7.9-8.9V36.6c0-5.9-3.6-11.4-7.9-12.4c-4.4-0.9-8,3.1-8,9v306.5
+		C43.5,345.6,47,351.1,51.4,352.1"/>
+	<path class="st8" d="M50.1,108.6H12.5c-2.3,0-4.1-1.8-4.1-4.1v-1c0-2.3,1.8-4.1,4.1-4.1h37.6c2.2,0,4.1,1.8,4.1,4.1v1
+		C54.2,106.8,52.3,108.6,50.1,108.6"/>
+	<path class="st8" d="M50.5,126.5H13c-2.3,0-4.1-1.8-4.1-4.1v-1c0-2.2,1.8-4.1,4.1-4.1h37.6c2.3,0,4.1,1.9,4.1,4.1v1
+		C54.6,124.7,52.8,126.5,50.5,126.5"/>
+	<path class="st8" d="M51,145.2H13.4c-2.2,0-4.1-1.8-4.1-4.1v-1c0-2.2,1.8-4.1,4.1-4.1H51c2.3,0,4.1,1.8,4.1,4.1v1
+		C55.1,143.3,53.3,145.2,51,145.2"/>
+	<rect y="264.5" class="st9" width="63.8" height="21.3"/>
+	<path class="st2" d="M99.3,104c5.5-5.6,14.5-5.6,20.1,0c5.5,5.5,5.5,14.5,0,20.1c-5.6,5.5-14.6,5.5-20.1,0
+		C93.7,118.5,93.7,109.5,99.3,104"/>
+	<path class="st10" d="M234.4,354.4L234.4,354.4c4.4-0.2,7.9-5.1,7.9-11V11.4c0-5.9-3.6-10.5-7.9-10.3c-4.4,0.2-7.9,5.2-7.9,11v332
+		C226.5,349.9,230,354.6,234.4,354.4"/>
+	<path class="st11" d="M331.6,345.6l2.8-0.6c4.3-0.9,7.2-5.1,6.4-9.5l-36.3-192.5c-0.9-4.4-5-7.2-9.4-6.3l-2.7,0.6
+		c-4.4,0.9-7.2,5.1-6.4,9.5l36.3,192.5C323.1,343.7,327.3,346.5,331.6,345.6"/>
+	<polygon class="st3" points="249.5,177.3 252.7,193.3 313.7,180.2 310.5,164.2 	"/>
+	<polygon class="st3" points="275.6,310.4 278.7,326.4 339.7,313.3 336.5,297.3 	"/>
+</g>
+</svg>
+</template>
