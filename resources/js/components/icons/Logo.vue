f55f60d8764d3b382ee858e68232245bc80b5347
Frank3TheTank3 <francisnic90@gmail.com>
2022-06-14 15:25:30 +0200

    Fix main logo
    Successfully cropped the logo and exportet to standard SVG Format.

--- resources/js/components/icons/Logo.vue
+++ resources/js/components/icons/Logo.vue
@@ -1,40 +1,16 @@
 <template>
-  <svg
-   width="210mm"
-   height="197mm"
-   viewBox="0 0 210 197"
+   <svg
+   width="181.38304mm"
+   height="170.98021mm"
+   viewBox="0 0 181.38304 170.98021"
    version="1.1"
    id="svg5"
-   inkscape:version="1.1.2 (0a00cf5339, 2022-02-04)"
-   sodipodi:docname="atomslogov2.svg"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
    xmlns:xlink="http://www.w3.org/1999/xlink"
    xmlns="http://www.w3.org/2000/svg"
    xmlns:svg="http://www.w3.org/2000/svg">
-  <sodipodi:namedview
-     id="namedview7"
-     pagecolor="#ffffff"
-     bordercolor="#666666"
-     borderopacity="1.0"
-     inkscape:pageshadow="2"
-     inkscape:pageopacity="0.0"
-     inkscape:pagecheckerboard="0"
-     inkscape:document-units="mm"
-     showgrid="false"
-     inkscape:zoom="0.69664703"
-     inkscape:cx="396.90114"
-     inkscape:cy="514.60781"
-     inkscape:window-width="1920"
-     inkscape:window-height="930"
-     inkscape:window-x="0"
-     inkscape:window-y="0"
-     inkscape:window-maximized="1"
-     inkscape:current-layer="layer1" />
   <defs
      id="defs2">
     <linearGradient
-       inkscape:collect="always"
        id="linearGradient5017">
       <stop
          style="stop-color:#dd0000;stop-opacity:1;"
@@ -46,7 +22,6 @@
          id="stop5015" />
     </linearGradient>
     <linearGradient
-       inkscape:collect="always"
        id="linearGradient4619">
       <stop
          style="stop-color:#49e3ff;stop-opacity:1"
@@ -58,7 +33,6 @@
          id="stop4617" />
     </linearGradient>
     <linearGradient
-       inkscape:collect="always"
        id="linearGradient3759">
       <stop
          style="stop-color:#dd0000;stop-opacity:1"
@@ -70,15 +44,13 @@
          id="stop3757" />
     </linearGradient>
     <linearGradient
-       id="linearGradient3715"
-       inkscape:swatch="solid">
+       id="linearGradient3715">
       <stop
          style="stop-color:#000000;stop-opacity:1;"
          offset="0"
          id="stop3713" />
     </linearGradient>
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient3759"
        id="linearGradient3761"
        x1="96.239563"
@@ -88,7 +60,6 @@
        gradientUnits="userSpaceOnUse"
        gradientTransform="translate(-7.3362822,-51.871743)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient4619"
        id="linearGradient4329"
        x1="80.559853"
@@ -98,7 +69,6 @@
        gradientUnits="userSpaceOnUse"
        gradientTransform="translate(-7.3362822,-51.871743)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient4619"
        id="linearGradient4519"
        x1="84.344467"
@@ -108,7 +78,6 @@
        gradientUnits="userSpaceOnUse"
        gradientTransform="translate(-7.3362822,-51.871743)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient4619"
        id="linearGradient4819"
        x1="100.07367"
@@ -118,7 +87,6 @@
        gradientUnits="userSpaceOnUse"
        gradientTransform="translate(-52.073635,5.5227919)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient5017"
        id="linearGradient5019"
        x1="97.891739"
@@ -128,7 +96,6 @@
        gradientUnits="userSpaceOnUse"
        gradientTransform="translate(-52.073635,5.5227919)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient5017"
        id="linearGradient5607"
        gradientUnits="userSpaceOnUse"
@@ -138,7 +105,6 @@
        y2="-105.21172"
        gradientTransform="translate(-154.95328,-40.543225)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient4619"
        id="linearGradient5609"
        gradientUnits="userSpaceOnUse"
@@ -148,7 +114,6 @@
        y2="-204.7813"
        gradientTransform="translate(-154.95328,-40.543225)" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient5017"
        id="linearGradient5670"
        gradientUnits="userSpaceOnUse"
@@ -158,7 +123,6 @@
        x2="100.07367"
        y2="-105.21172" />
     <linearGradient
-       inkscape:collect="always"
        xlink:href="#linearGradient4619"
        id="linearGradient5672"
        gradientUnits="userSpaceOnUse"
@@ -169,9 +133,8 @@
        y2="-204.7813" />
   </defs>
   <g
-     inkscape:label="Layer 1"
-     inkscape:groupmode="layer"
-     id="layer1">
+     id="layer1"
+     transform="translate(-12.539393,-12.52901)">
     <ellipse
        style="fill:none;fill-opacity:0.858824;stroke:url(#linearGradient4519);stroke-width:5.41601;stroke-miterlimit:4;stroke-dasharray:none"
        id="path846"
@@ -212,5 +175,4 @@
        ry="25.422554" />
   </g>
 </svg>
-   
 </template>
