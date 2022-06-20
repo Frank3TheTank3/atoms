f59a9b43aad864d2c87bfcbe26af2037b62120c7
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2022-06-20 11:42:12 +0200

    Update Profile page layout
    Make it work with the rest of the design

---
+++ resources/js/Layouts/ProfileLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <BaseLayout :title="title">
+        <template #main>
+            <div class="bg-black py-20">
+                <slot />
+            </div>
+        </template>
+    </BaseLayout>
+</template>
+
+<script setup>
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+
+defineProps({
+    title : String
+});
+</script>
