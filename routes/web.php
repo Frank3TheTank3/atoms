21ebfb69b48757cfac931bcebfff5d0bf620634f
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2022-06-20 10:45:11 +0200

    Remove dashboard
    Remove default dashboard, route and set HOME to landing page

--- routes/web.php
+++ routes/web.php
@@ -28,13 +28,3 @@ Route::get('/', function () {
 
 Route::resource('atoms', AtomsController::class);
 Route::resource('atoms.comments', AtomCommentsController::class);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
