cc162bd17a96fa7bf231c1d3a5f44113c7f43944
Frank3TheTank3 <francisnic90@gmail.com>
2022-06-20 15:13:00 +0200

    Fix rolling atom component
    Added vue reference

--- resources/js/components/RollingAtom.vue
+++ resources/js/components/RollingAtom.vue
@@ -1,25 +1,41 @@
 <template>
-    <div id="myContainer" class="h-3 w-full relative bg-black">
-        <div class="rotateR w-12 h-12 absolute bottom-3" id="myAnim">
+    <div
+        ref="myContainer"
+        id="myContainer"
+        class="h-3 w-full relative bg-black"
+    >
+        <div
+            class="rotateR w-12 h-12 absolute bottom-3"
+            ref="myAnim"
+            id="myAnim"
+        >
             <ClipartAtomic></ClipartAtomic>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import ClipartAtomic from "@/components/icons/ClipartAtomic.vue";
+const myContainer = ref(null);
+const myAnim = ref(null);
 
 let id = null;
 
 function myMove() {
+
     let movingLeft = true;
     let pos = 0;
     clearInterval(id);
     id = setInterval(frame, 2);
 
     function frame() {
-        let width = document.getElementById("myContainer").clientWidth - 50;
-        let elem = document.getElementById("myAnim");
+        let cont = myContainer.value;
+        let width;
+        if ((cont = !null)) {
+            width = cont.clientWidth - 50;
+        }
+        let elem = myAnim;
 
         if (movingLeft) {
             pos++;
@@ -28,9 +44,10 @@ function myMove() {
         if (!movingLeft) {
             pos--;
         }
-
-        elem.style.left = pos + "px";
-
+        if(elem.value != null)
+        {
+        elem.value.style.left = pos + "px";
+}
         if (pos >= width && movingLeft) {
             movingLeft = false;
             elem.classList.replace("rotateR", "rotateL");
