d1bf9f7de98816f1321e3515db247648ebfc5c16
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2022-06-20 14:37:15 +0200

    Colorize PTable based on atom series
    Set PTable cells background colors based on atom series.

--- resources/js/components/PTableCell.vue
+++ resources/js/components/PTableCell.vue
@@ -1,7 +1,7 @@
 <template>
     <Link
         :href="route('atoms.show', { id: data?.id })"
-        class="flex flex-col bg-red-400"
+        class="flex flex-col" :class="bgColor"
     >
         <div class="text-xs">{{ data?.order }}</div>
         <div class="text-xl font-black">{{ data?.symbol }}</div>
@@ -10,9 +10,23 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
 import { Link } from "@inertiajs/inertia-vue3";
 
-defineProps({
+const props = defineProps({
     data: Object,
 });
+
+const bgColor = computed(() => ({
+    'bg-atom-alkali': props.data.series === 'alkali_metals',
+    'bg-atom-alkali-earth': props.data.series === 'alkaline_earth_metals',
+    'bg-atom-lanthanoids': props.data.series === 'lanthanoids',
+    'bg-atom-actinoids': props.data.series === 'actinoids',
+    'bg-atom-transition': props.data.series === 'transition_metals',
+    'bg-atom-post-transition': props.data.series === 'post-transition_metals',
+    'bg-atom-metalloids': props.data.series === 'metalloids',
+    'bg-atom-reactive-non': props.data.series === 'reactive_nonmetals',
+    'bg-atom-noble': props.data.series === 'noble_gasses',
+    'bg-atom-undef': props.data.series === null
+}))
 </script>
